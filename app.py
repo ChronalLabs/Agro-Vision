de52bb9bc551ee2e08199aa24021ce14bc2e5cb7
Manan chawla <113688405+Manan-Chawla@users.noreply.github.com>
2026-02-14 09:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,6 +102,7 @@ def get_forecast():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)True)
+    app.run(host="0.0.0.0", port=port,debug=True)
+
 
 
